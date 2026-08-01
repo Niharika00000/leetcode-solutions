6b186554c9e60c9716b93007d1b396352311ddcc
Niharika Sharma <150238540+Niharika00000@users.noreply.github.com>
2026-08-01 21:47:36 +0530

    Time: 0 ms (100%), Space: 42.5 MB (89.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,29 +1,17 @@
 class Solution {
 
     public boolean predictTheWinner(int[] nums) {
-
+        
         int n = nums.length;
-        int[][] dp = new int[n][n];
-
-        // Base case
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = nums[i];
-        }
-
-        // Fill DP table
-        for (int len = 2; len <= n; len++) {
-
-            for (int i = 0; i <= n - len; i++) {
-
-                int j = i + len - 1;
-
-                int takeLeft = nums[i] - dp[i + 1][j];
-                int takeRight = nums[j] - dp[i][j - 1];
-
-                dp[i][j] = Math.max(takeLeft, takeRight);
+        int[] dp = nums.clone(); 
+        
+        for (int diff = 1; diff < n; ++diff) {
+            for (int j = n - 1; j - diff >= 0; --j) {
+                int i = j - diff;
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
             }
         }
-
-        return dp[0][n - 1] >= 0;
+        
+        return dp[n - 1] >= 0;
     }
 }
