7a6b17111043f1ceed59b5a5e79e28e138a575d6
Niharika Sharma <150238540+Niharika00000@users.noreply.github.com>
2026-08-19 16:18:10 +0530

    Time: 28 ms (7.34%), Space: 106.8 MB (64.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,13 +3,17 @@ class Solution {
         HashMap <Integer,Integer> freq = new HashMap<>();
             for(int num:nums){
                 freq.put(num,freq.getOrDefault(num,0)+1);
-                
-            }
-            for(int key:freq.keySet()){
-                 if(freq.get(key)>1)
+
+                if(freq.get(num)>1){
                     return true;
+                }
                 
             }
+            // for(int key:freq.keySet()){
+            //      if(freq.get(key)>1)
+            //         return true;
+                
+            // }
             
         return false;
         
