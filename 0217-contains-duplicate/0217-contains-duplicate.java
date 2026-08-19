f56415a1b4f118ec4ea6df6474b843a0c1b23de3
Niharika Sharma <150238540+Niharika00000@users.noreply.github.com>
2026-08-19 16:16:42 +0530

    Time: 43 ms (5.1%), Space: 110.2 MB (9.87%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap <Integer,Integer> freq = new HashMap<>();
+            for(int num:nums){
+                freq.put(num,freq.getOrDefault(num,0)+1);
+                
+            }
+            for(int key:freq.keySet()){
+                 if(freq.get(key)>1)
+                    return true;
+                
+            }
+            
+        return false;
+        
+    }
+}
